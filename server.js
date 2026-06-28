1c012ab49954d54586653c06d6bcc0f0cce6f7d9
root <songvc@gmail.com>
2026-06-27 23:29:12 -0700

    change server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import path from "path";
 import fs from "fs";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const uploadDir = path.join(process.cwd(), "public/uploads");
 
 fs.mkdirSync(uploadDir, { recursive: true });
